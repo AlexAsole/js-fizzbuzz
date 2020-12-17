1fbf7e033da44e5e8590972cb6c10ee6efcc4110
Alex Giorgio Asole <alexgiorgio.asole@gmail.com>
2020-12-17 15:26:05 +0100

    Completo nel log

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+var number = 0;   //numero base a cui addizionero 1 in maniera ciclica, fino a 100
+for (var i = 0; i < 100; i++) {   //ciclo di 100 operazioni
+  number += 1;
+  if (number % 3 === 0 || number % 5 === 0) {   //se è divisibile per 3 O 5 allora controlla:
+    if (number % 3 === 0 && number % 5 === 0){  //se è divisibile per 3 E 5
+      console.log('FizzBuzz')
+    } else if (number % 3 === 0){   //se è divisibile SOLO per 3
+      console.log('Buzz')
+    } else if (number % 5 === 0) {  //se è divisibile SOLO per 5
+      console.log('Fizz')
+    }
+  } else {  //se non è divisibile per nessuno dei due
+    console.log(number)
+  }
+}
